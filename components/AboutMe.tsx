52e7d1c2110708c61570385b1e3c34bf66579665
Azamat Kurbanov <azik1kurbanov@gmail.com>
2023-12-25 15:04:34 +0000

    update

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -84,15 +84,15 @@ export default function AboutMe() {
               Social Links
             </h1>
             <div className="mt-4 ml-4">
-              <div className="flex flex-row justify-start items-center ">
+              <div className="flex flex-row justify-start items-center">
                 <Link
-                  href={userData.socialLinks.peerlist}
+                  href={userData.socialLinks.linkedin}
                   className="flex flex-row items-center space-x-4 group"
                 >
                   <div className="my-4">&rarr;</div>
                   <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
                     <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
-                    Peerlist
+                    LinkedIn
                   </div>
                 </Link>
               </div>
@@ -134,18 +134,6 @@ export default function AboutMe() {
               </div>
               <div className="flex flex-row justify-start items-center">
                 <Link
-                  href={userData.socialLinks.linkedin}
-                  className="flex flex-row items-center space-x-4 group"
-                >
-                  <div className="my-4">&rarr;</div>
-                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
-                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
-                    LinkedIn
-                  </div>
-                </Link>
-              </div>
-              <div className="flex flex-row justify-start items-center">
-                <Link
                   href={userData.socialLinks.instagram}
                   className="flex flex-row items-center space-x-4 group"
                 >
@@ -153,6 +141,18 @@ export default function AboutMe() {
                   <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
                     <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-28 group-hover:translate-x-0 transition duration-300"></div>
                     Instagram
+                  </div>
+                </Link>
+              </div>
+              <div className="flex flex-row justify-start items-center ">
+                <Link
+                  href={userData.socialLinks.peerlist}
+                  className="flex flex-row items-center space-x-4 group"
+                >
+                  <div className="my-4">&rarr;</div>
+                  <div className="text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300">
+                    <div className="absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-24 group-hover:translate-x-0 transition duration-300"></div>
+                    Peerlist
                   </div>
                 </Link>
               </div>
